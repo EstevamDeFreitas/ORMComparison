af0a524ef29beb8a85eff2c109d49663f310b4d4
Estevam De Freitas <estevamdefreitas37@gmail.com>
2023-04-03 19:19:25 -0300

    adicionado

---
+++ Entidades/CursoAluno.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class CursoAluno
+    {
+        public Guid AlunoId { get; set; }
+        public Guid CursoId { get; set; }
+        public uint Nota { get; set; }
+    }
+}
