6189edd089feac91467ac6715fcb631921f5336f
Francisco de Souza Rangel <franciscosouza20092000@gmail.com>
2023-04-04 16:55:58 -0300

    EfCore rodando

--- EFCore/EFCoreMain.cs
+++ EFCore/EFCoreMain.cs
@@ -1,32 +1,54 @@
 ﻿using EFCore;
 using Entidades;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 
 namespace EFCore
 {
     public class EFCoreMain
     {
-        private string ConnectionString { get; set; }
+        private static readonly string _stringConexao
+        = "Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=ORMComparison;Data Source=DESKTOP-GPE9S1B\\SQLEXPRESS";
 
-        public EFCoreMain(string connectionString)
+        private static DbContextOptions<MeuContexto> ObterOpcoesDbContext()
         {
-            ConnectionString = connectionString;
+            var builder = new SqlConnectionStringBuilder(_stringConexao);
+
+            var optionsBuilder = new DbContextOptionsBuilder<MeuContexto>();
+            optionsBuilder.UseSqlServer(builder.ConnectionString);
+
+            return optionsBuilder.Options;
         }
 
-    }
-
-    public class MeuContexto : DbContext
-    {
-        public MeuContexto(DbContextOptions<MeuContexto> options) : base(options)
+        public static void Main()
         {
+            using (var contexto = new MeuContexto())
+            {
+                /*
+                // Cria uma nova instância de uma entidade e salva no banco de dados
+                var curso = new Curso
+                {
+                    Nome = "Introdução ao EF Core",
+                    Descricao = "Curso de introdução ao EF Core"
+                };
+
+                contexto.Cursos.Add(curso);
+                contexto.SaveChanges();*/
+            }
         }
 
-        public DbSet<Curso> Cursos { get; set; }
-        public DbSet<CursoAluno> CursoAlunos { get; set; }
-        public DbSet<Endereco> Enderecos { get; set; }
-        public DbSet<Estudante> Estudantes { get; set; }
-        public DbSet<Pessoa> Pessoas { get; set; }
-        public DbSet<Professor> Professores { get; set; }
+        public class MeuContexto : DbContext
+        {
+            public MeuContexto() : base(ObterOpcoesDbContext())
+            {
+            }
 
+            public DbSet<Curso> Cursos { get; set; }
+            public DbSet<CursoAluno> CursoAlunos { get; set; }
+            public DbSet<Endereco> Enderecos { get; set; }
+            public DbSet<Estudante> Estudantes { get; set; }
+            public DbSet<Pessoa> Pessoas { get; set; }
+            public DbSet<Professor> Professores { get; set; }
+        }
     }
 }
--- ORMComparisson/Program.cs
+++ ORMComparisson/Program.cs
@@ -1,12 +1,16 @@
 ﻿
 using ADONET;
+using EFCore;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
+        /*
         AdoNetMain adomain = new AdoNetMain("Data Source=DESKTOP-L42IOG5;Initial Catalog=orm_comparissondb;User ID=orm_user;Password=123456");
 
-        adomain.InitTest();
+        adomain.InitTest();*/
+
+        EFCoreMain.Main();
     }
 }
