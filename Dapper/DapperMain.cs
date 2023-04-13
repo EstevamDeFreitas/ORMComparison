46b639c3d514ef535f9a881d9edf9590210e7d46
MateRamires <matheushenriqueramires@gmail.com>
2023-04-13 18:22:05 -0300

    Insercao com o dapper

--- Dapper/DapperMain.cs
+++ Dapper/DapperMain.cs
@@ -1,12 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Entidades;
+using System.Data.SqlClient;
 
-namespace Dapper
+namespace Dapper;
+
+public class DapperMain
 {
-    internal class DapperMain
+    private string _connectionString;
+    public DapperMain(string connectionString) 
+    { 
+        _connectionString = connectionString;
+    }
+
+    public void InitTest()
     {
+        SqlConnection connection = new SqlConnection(_connectionString);
+
+        connection.Open();
+
+        RunInsertTest(connection);
+    }
+
+    public void RunInsertTest(SqlConnection conn) 
+    {
+
+        var endereco = new Endereco
+        {
+            Id = Guid.NewGuid(),
+            Pais = "Brasil",
+            Estado = "São Paulo",
+            Cidade = "São Paulo",
+            Rua = "Rua Teste",
+            Numero = "421"
+        };
+
+        string sql = "INSERT INTO Enderecos (Id, Pais, Estado, Cidade, Rua, Numero) VALUES (@Id, @Pais, @Estado, @Cidade, @Rua, @Numero)";
+
+        int affectedRows = conn.Execute(sql, endereco);
+
+        Console.WriteLine(affectedRows);
+
     }
 }
